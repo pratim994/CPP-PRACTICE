bd5bc1afc5b234d2c7d16a0777c5f41960c9377e
pratim chakraborty <aryanpratim86@gmail.com>
2026-01-23 08:54:30 +0530

    Add files via upload

---
+++ vb.cpp
@@ -0,0 +1,35 @@
+//solution for 2167 D//
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+using ll = long long;
+	int t;
+cin >>t;
+
+while(t--){
+	int n;
+	cin >> n;
+	vector<ll> a(n);
+	for(auto &i : a) cin >> i;
+	for(ll x :{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53}){
+		int ok = 0;
+		for(ll i : a ){
+			if(i%x){
+				ok = 1;
+				break;
+			}
+		}
+		if(ok){
+			cout << x <<'\n';
+			break;
+		}
+	}
+	
+	}
+
+return 0;
+
+}
