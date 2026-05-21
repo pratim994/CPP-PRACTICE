ff421797e7f059bc0d8126675a85a25bc6d5a43b
pratim <aryanpratim86@gmail.com>
2026-05-21 19:54:51 +0530

    leetcode 200 solution

---
+++ codeforces/flip_the_bit_easy.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t;
+
+	std::cin >> t;
+
+	while(t--)
+	{
+		int n, k, p, ans = 0;
+
+		std::cin >> n >> k;
+
+		std::vector<int> arr(n);
+
+		for(auto &i : arr) std::cin >> i ;
+
+		std::cin >> p;
+
+	}
+}	
