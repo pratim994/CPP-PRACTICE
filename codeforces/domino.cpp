d6ba19ec8b9dff878b33e9d7cc5e5ae1ed214f06
pratim <aryanpratim86@gmail.com>
2026-08-10 23:28:52 +0530

    hard day but fruitful one , praise to saturn

---
+++ codeforces/domino.cpp
@@ -0,0 +1,20 @@
+// codeforces 50A domino piling  
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	
+	int n, m;
+
+	scanf("%d %d" , &n, &m);
+
+
+	cout << std::floor((n*m)/2) << '\n';
+
+	return 0;
+
+
+}
